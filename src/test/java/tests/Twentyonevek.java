81feadcc5ecef368e8c645abf0303e10001f505e
AGordey <gordey.andrej@gmail.com>
2022-07-24 22:26:34 +0300

    Diplom part 3 Mobile

--- src/test/java/tests/Twentyonevek.java
+++ src/test/java/tests/Twentyonevek.java
@@ -1,64 +1,66 @@
 package tests;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.step;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 @Tag("21vek")
 public class Twentyonevek extends TestBase {
     @Test
-    @DisplayName("Элементы главной страницы")
+    @DisplayName("Наличие элементов главной страницы")
     void searchTest() {
 
         $(AppiumBy.xpath("//android.view.View[@content-desc=\"Все акции\"]/android.widget.TextView")).shouldHave(text("Все акции"));
         $$(AppiumBy.className("android.widget.Button")).findBy(text("Каталог")).shouldHave(text("Каталог"));
+        $$(AppiumBy.className("android.widget.Button")).findBy(text("Search")).shouldHave(text("Search"));
+        $$(AppiumBy.className("android.view.View")).findBy(text("ПОПУЛЯРНЫЕ ТОВАРЫ")).shouldHave(text("ПОПУЛЯРНЫЕ ТОВАРЫ"));
+        $$(AppiumBy.className("android.webkit.WebView")).findBy(text("Онлайн-гипермаркет 21vek.by")).shouldHave(text("Онлайн-гипермаркет 21vek.by"));
 
     }
 
     @Test
-    @Disabled
+    @DisplayName("Открытие вкладки Все Акции")
     void searchTest2() {
 
         $(AppiumBy.xpath("//android.view.View[@content-desc=\"Все акции\"]")).click();
         $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[1]/android.view.View[1]/android.view.View[2]/android.view.View[2]")).shouldHave(text("Все акции"));
+        $$(AppiumBy.className("android.widget.Button")).findBy(text("Категории")).shouldBe(visible);
+        $$(AppiumBy.className("android.widget.Button")).findBy(text("Фильтр")).shouldBe(visible);
+    }
 
-    }   @Test
-    @Disabled
+    @Test
+    @DisplayName("Открытие вкладки Каталог")
     void searchTest3() {
 
         $$(AppiumBy.className("android.widget.Button")).findBy(text("Каталог")).click();
         $$(AppiumBy.className("android.view.View")).findBy(text("Каталог товаров")).shouldHave(text("Каталог товаров"));
 
     }
-//        String searchValue = "Ubuntu";
-//        back();
-//        step("Type search", () -> {
-//            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
-//            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys(searchValue);
-//        });
-//        step("Verify content found", () -> {
-//            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-//                    .shouldHave(CollectionCondition.sizeGreaterThan(0));
-//            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-//                    .shouldHave(text(searchValue));
-//            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_description"))
-//                    .shouldHave(text("Linux distribution developed by Canonical Ltd."));
-//        });
-////          Проходит на Pixel 6
-//        step("Open page with article about" + searchValue, () -> {
-//            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-//                    .find(Condition.text(searchValue)).click();
-//        });
-//        step("Verify article about " + searchValue, () -> {
-//            $(AppiumBy.className("android.view.View")).shouldHave(text(searchValue));
-//       });
-//    }
+
+    @Test
+    @DisplayName("Наличие разделов в Каталоге")
+    void searchTest4() {
+        $$(AppiumBy.className("android.widget.Button")).findBy(text("Каталог")).click();
+        $$(AppiumBy.className("android.view.View")).findBy(text("Все акции")).shouldBe(visible);
+        $$(AppiumBy.className("android.view.View")).findBy(text("Бытовая техника")).shouldBe(visible);
+        $$(AppiumBy.className("android.view.View")).findBy(text("Смартфоны, ТВ и электроника")).shouldBe(visible);
+        $$(AppiumBy.className("android.view.View")).findBy(text("Компьютеры и периферия")).shouldBe(visible);
+        $$(AppiumBy.className("android.view.View")).findBy(text("Мебель")).shouldBe(visible);
+        $$(AppiumBy.className("android.view.View")).findBy(text("Товары для дома")).shouldBe(visible);
+
+    }
+    @Test
+    @DisplayName("Добавление товара из главной страницы в корзину")
+    void searchTest5() {
+        $$(AppiumBy.className("android.widget.Button")).findBy(text("В корзину")).click();
+        $$(AppiumBy.className("android.widget.TextView")).findBy(text("1")).shouldBe(visible);
+
+    }
 }
