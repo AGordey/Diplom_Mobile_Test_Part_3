b08e549dcccc650960dd67475caf7bbe78be1733
AGordey <gordey.andrej@gmail.com>
2022-07-25 16:35:00 +0300

    Diplom part 3 Mobile

--- src/test/java/tests/Twentyonevek.java
+++ src/test/java/tests/Twentyonevek.java
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.Selenide.$$;
 public class Twentyonevek extends TestBase {
     @Test
     @DisplayName("Наличие элементов главной страницы")
-    void searchTest() {
+    void elementsOnMainPageTest() {
 
         $(AppiumBy.xpath("//android.view.View[@content-desc=\"Все акции\"]/android.widget.TextView")).shouldHave(text("Все акции"));
         $$(AppiumBy.className("android.widget.Button")).findBy(text("Каталог")).shouldHave(text("Каталог"));
@@ -24,7 +24,7 @@ public class Twentyonevek extends TestBase {
 
     @Test
     @DisplayName("Открытие вкладки Все Акции")
-    void searchTest2() {
+    void tabAllPromotionsTest() {
 
         $(AppiumBy.xpath("//android.view.View[@content-desc=\"Все акции\"]")).click();
         $$(AppiumBy.className("android.widget.TextView")).findBy(text("Все акции")).shouldBe(visible);
@@ -34,7 +34,7 @@ public class Twentyonevek extends TestBase {
 
     @Test
     @DisplayName("Открытие вкладки Каталог")
-    void searchTest3() {
+    void tabCatalogTest() {
 
         $$(AppiumBy.className("android.widget.Button")).findBy(text("Каталог")).click();
         $$(AppiumBy.className("android.widget.TextView")).findBy(text("Каталог товаров")).shouldBe(visible);
@@ -43,7 +43,7 @@ public class Twentyonevek extends TestBase {
 
     @Test
     @DisplayName("Наличие разделов в Каталоге")
-    void searchTest4() {
+    void sectionInCatalogTest() {
         $$(AppiumBy.className("android.widget.Button")).findBy(text("Каталог")).click();
         $$(AppiumBy.className("android.widget.TextView")).findBy(text("Все акции")).shouldBe(visible);
         $$(AppiumBy.className("android.widget.TextView")).findBy(text("Бытовая техника")).shouldBe(visible);
@@ -56,7 +56,7 @@ public class Twentyonevek extends TestBase {
 
     @Test
     @DisplayName("Добавление товара из главной страницы в корзину")
-    void searchTest5() {
+    void addProductInCardTest() {
 
 
         $$(AppiumBy.className("android.view.View")).findBy(text("Подгузники-трусики детские Pampers Pants 5 Junior (150шт)")).click();
